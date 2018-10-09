76b309070201674bb0f856a8b22ab16d49d9bd85
youli-jlu <38717633+youli-jlu@users.noreply.github.com>
2018-10-09 18:23:05 +0800

    Create string-as-filename.cpp

---
+++ string-as-filename.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+//How to attach a string argument with a integer/float/string in a C++ way !!!!!
+//The main idea is using stringstream and connnect them by stream
+//How convinient c++ is to do this fortran-dirty job!!!!
+//If you want to use the method, you need to include <sstream> and <string>
+using namespace std;
+
+int main()
+{
+    int i=10;
+    ostringstream jstep;
+
+      jstep << i << "step.dat";
+
+    ofstream out;
+    char ch[100];
+    string str=jstep.str();
+    strcpy(ch,str.c_str());
+//    string str="yestets2";
+//    string str=jstep.str();
+
+   // strcpy(a,jstep.str());
+    out.open(ch);
+    out << "you are right" << endl;
+    return 0;
+}
